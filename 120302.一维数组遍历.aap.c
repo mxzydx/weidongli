f2405a73bb0db6d9ee56c18e683cd5d586991ecf
mxzydx <74296149+mxzydx@users.noreply.github.com>
2020-12-03 21:09:38 +0800

    Create 120302.一维数组遍历.aap.c

---
+++ 120302.一维数组遍历.aap.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+int a;
+int array[]={2050,2051,2052,2053};
+int array2d[2][5]={
+	{1,2,3,4,5},
+	{6,7,8,9,10}
+
+};
+
+for(a=0;a<4;a++){
+	
+		printf("%d ",array[a]);
+	
+	}
+
+printf("\n");
+
+}
